bb448d14c3505b90a8c6a3a504c1970dc9cb3c96
yunabc <450387582@qq.com>
2017-06-07 18:35:13 +0800

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 module.exports = {
+	// context:'/'
 	entry:'./src/script/main.js',
 	output:{
-		path:path.resolve(__dirname,'dist/js'),
-		filename:'bundle.js'
-	}
+		path:path.resolve(__dirname,'dist'),
+		filename:'js/[name]-[chunkhash].js'
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			template:'index.html',
+			filename:'index-[hash].html',
+			inject:'head' // script 标签的位置 
+
+		})
+	]
 };
